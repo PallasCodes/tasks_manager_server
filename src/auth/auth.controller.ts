0663199c714a49d4773426faca3c5685926ff4e9
pallascodes <pallascodes@gmail.com>
2025-05-20 16:36:47 -0600

    feat(auth): implement user registration and login functionality
    - Added `register` and `login` methods in `AuthService` to handle user registration and authentication.
    - Created DTOs `CreateUserDto` and `LoginUserDto` for user input validation.
    - Implemented JWT strategy for authentication using `passport-jwt`.
    - Added `UserRoleGuard` to protect routes based on user roles.
    - Updated `AuthController` to include endpoints for user registration and login.
    - Enhanced `AuthModule` to include necessary imports and providers for JWT and Passport.
    - Added decorators for role-based access control.
    - Updated `package.json` and `package-lock.json` to include new dependencies.
    - Created HTTP request examples for registration and login in `auth.http`.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { ApiResponse } from '@nestjs/swagger'
+
+import { AuthService } from './auth.service'
+import { CreateUserDto, LoginUserDto } from './dto'
+import { User } from './entities/user.entity'
+import { Auth } from './decorators'
+import { ValidRoles } from './interfaces/valid-roles.interface'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @ApiResponse({ status: 201, description: 'User was registered', type: User })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @Post('register')
+  register(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto)
+  }
+
+  @ApiResponse({ status: 201, description: 'User was logged in', type: User })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto)
+  }
 }
