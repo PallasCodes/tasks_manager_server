d1c6d0d37c649042c7e02c0c36c6ad7661cf8ce3
pallascodes <pallascodes@gmail.com>
2025-08-28 10:36:53 -0600

    test(tasks): enhance unit tests setup for TasksService with mock dependencies

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,18 +1,48 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { TasksService } from './tasks.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+
+import { ListsService } from '../lists/lists.service'
+import { Task } from './entities/task.entity'
+import { TasksService } from './tasks.service'
 
 describe('TasksService', () => {
-  let service: TasksService;
+  let service: TasksService
+  let taskRepository: Repository<Task>
+  let listService: ListsService
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
-    }).compile();
+    const mockTaskRepository: Partial<Repository<Task>> = {
+      save: jest.fn(),
+      count: jest.fn(),
+      find: jest.fn(),
+      countBy: jest.fn(),
+      merge: jest.fn(),
+      remove: jest.fn()
+    }
 
-    service = module.get<TasksService>(TasksService);
-  });
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        {
+          provide: getRepositoryToken(Task),
+          useValue: mockTaskRepository
+        },
+        {
+          provide: ListsService,
+          useValue: {
+            findOne: jest.fn()
+          }
+        }
+      ]
+    }).compile()
+
+    service = module.get<TasksService>(TasksService)
+    taskRepository = module.get<Repository<Task>>(getRepositoryToken(Task))
+    listService = module.get<ListsService>(ListsService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
